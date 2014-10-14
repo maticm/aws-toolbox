22d74d2ab35f8cee48e37128a9c2e5c0ae2d3774
maticm <matic.makovec@gmail.com>
2014-10-14 13:50:42 +0200

    No need for extended information

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-set -eux -o pipefail
+set -eu -o pipefail
 
 fail() {
     echo "$(basename $0): error: $1" >&2
